16aac3c9f3f353bd16712cecc86139f55321c2ed
Chriscastanuela <chriscastanuela@ymail.com>
2023-12-15 23:10:37 -0700

    add form

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -15,7 +15,7 @@ const ContactUs = () => {
                 <div className="w-7/12 h-full p-3 md:w-full px-9">
                     <h2 className="text-2xl py-7">Get In Touch</h2>
                     <div className="">
-                        <form action="" className="flex flex-col gap-y-4" method="post" name='contact' data-netlify='true' data-netlify-success={console.log(window.location.pathname) && `https://www.kjsautohailrepair.com/${window.location.pathname}`}>
+                        <form action="" className="flex flex-col gap-y-4" method="post" name='contact' netlify id='contact'>
                             <div className="flex gap-x-4">
                                 <input type="text" name='name' placeholder="Name" className="w-full pl-2 py-2 outline-none  rounded-md border border-gray-300 " />
                             </div>
