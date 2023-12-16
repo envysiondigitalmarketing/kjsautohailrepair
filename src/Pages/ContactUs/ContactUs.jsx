5674287d213cc6ec507ebee96ef943c3bae65ddd
Chriscastanuela <chriscastanuela@ymail.com>
2023-12-15 23:48:43 -0700

    final form

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -15,7 +15,7 @@ const ContactUs = () => {
                 <div className="w-7/12 h-full p-3 md:w-full px-9">
                     <h2 className="text-2xl py-7">Get In Touch</h2>
                     <div className="">
-                        <form name='contact' action="/Contact" method="post" netlify className="flex flex-col gap-y-4" id='contact'>
+                        <form name='contact' action="/contact" method="post" netlify className="flex flex-col gap-y-4" id='contact'>
                             <input type="hidden" name="form-name" value="contact" />
                             <div className="flex gap-x-4">
                                 <input type="text" name='name' placeholder="Name" className="w-full pl-2 py-2 outline-none  rounded-md border border-gray-300 " />
